55963c034824079a853976fcbe1522c674b6a017
julio <jc2alvarado@gmail.com>
2022-02-22 14:16:29 -0800

    uplaoding to github

--- src/App.js
+++ src/App.js
@@ -1,23 +1,24 @@
-import logo from './logo.svg';
+
 import './App.css';
+import Home from "./Home"
+import WhiskeyList from './WhiskeyList';
+import "bootstrap"
+import "bootstrap/dist/css/bootstrap.min.css"
+import {Routes,Route} from "react-router-dom"
+import seedReview from "./seedReview";
+import Review from './Review';
+import NewReviewForm from './NewReviewForm';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Routes>
+        <Route exact path="/whiskey/:id" element={<Review reviews={seedReview} /> }/>
+        <Route exact path="/whiskeys" element={<WhiskeyList reviews={seedReview}/>} />
+        <Route exact path="/createReview" element={<NewReviewForm reviews={seedReview}/>} />
+        <Route exact path="/" element={<Home/>}/>
+        
+      </Routes>
     </div>
   );
 }
